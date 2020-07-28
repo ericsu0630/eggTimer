1176fbceb4c529835606f12f5fe5702c72fce68b
sushiz <sushiz@users.noreply.github.com>
2020-07-28 23:04:01 +0800

    Simple Android Timer

--- app/src/main/java/com/example/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/timerdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.timerdemo;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,71 +16,79 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView textView;
+    Button button;
+    CountDownTimer timer;
+    boolean timerIsActive = false;
+    int maxTime;
 
+    @SuppressLint("DefaultLocale")
     public String secondsToString(int pTime) {
-        String time = String.format("%02d:%02d", pTime / 60, pTime % 60);
-        return time;
+        return String.format("%02d:%02d", pTime / 60, pTime % 60);
+    }
+
+    public void resetTimer(){
+        timer.cancel();
+        seekBar.setProgress(maxTime);
+        seekBar.setEnabled(true);
+        button.setText(R.string.startButton_text);
+        timerIsActive=false;
+    }
+
+    public void startStopTimer(View view){
+        if(timerIsActive){
+            resetTimer();
+        }
+        else {
+            seekBar.setEnabled(false);
+            button.setText(R.string.stopButton_text);
+            timer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+                @Override
+                public void onTick(long msUntilEnd) {
+                    seekBar.setProgress((int) msUntilEnd / 1000);
+                    textView.setText(secondsToString((int) msUntilEnd / 1000));
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer media = MediaPlayer.create(getApplicationContext(), R.raw.partyhorn);
+                    media.start();
+                    resetTimer();
+                }
+            };
+            timer.start();
+            timerIsActive=true;
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final SeekBar seekbar = findViewById(R.id.seekBar);
-        final TextView textView = findViewById(R.id.counterTextView);
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.partyhorn);
-        final Button button = findViewById(R.id.startStopButton);
-        final int maxTime = 300; //5minutes
-        seekbar.setMax(maxTime);
-        seekbar.setProgress(maxTime); //set default progress to max time
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.counterTextView);
+        button = findViewById(R.id.startStopButton);
+        maxTime = 300; //5min
+        seekBar.setMax(maxTime);
+        seekBar.setProgress(maxTime);
         textView.setText(secondsToString(maxTime));
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                String minutes = secondsToString(seekBar.getProgress());
-                textView.setText(minutes);
+                textView.setText(secondsToString(seekBar.getProgress()));
             }
 
-            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                button.setFocusable(View.NOT_FOCUSABLE);
+
             }
 
-            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                button.setFocusable(View.FOCUSABLE);
-            }
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int progress = seekbar.getProgress();
-                CountDownTimer timer = new CountDownTimer(progress*1000,1000) {
-                    @Override
-                    public void onTick(long msUntilEnd) {
-                        int secondsLeft = (int)msUntilEnd/1000;
-                        seekbar.setProgress(secondsLeft);
-                    }
 
-                    @Override
-                    public void onFinish() {
-                        mediaPlayer.start();
-                    }
-                };
-                timer.start();
             }
         });
-//        final Handler handle = new Handler();
-//        Runnable run = new Runnable() {
-//            @Override
-//            public void run() {
-//                Log.i("info","A second passed");
-//                handle.postDelayed(this,1000);
-//            }
-//        };
-//        handle.post(run);
     }
 
 
